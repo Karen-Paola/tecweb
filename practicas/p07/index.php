8272514425f0161567cce307e9f530263e661943
Karen <paredeskrnm@gmai.com>
2024-09-10 14:46:04 -0600

    Se llamó a la segunda función

--- practicas/p07/index.php
+++ practicas/p07/index.php
@@ -11,18 +11,13 @@
     <?php
     include 'src/funciones.php';
     esMultiploDe5Y7($_GET['numero']);
-        /*if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
-        }*/
+    ?>
+
+    <h2>Ejercicio 2</h2>
+    <p>Secuencia de números aleatorios:<p>
+    <?php
+    include 'src/funciones.php';
+    repetirNumAleatorios();
     ?>
 
     <h2>Ejemplo de POST</h2>
