489ac04ff18fa3e179c45592ce78003f73e88342
Karen <paredeskrnm@gmai.com>
2024-10-06 14:00:09 -0600

    Se modificó get_productos_xhtml_v2

--- practicas/p10/get_productos_xhtml_v2.php
+++ practicas/p10/get_productos_xhtml_v2.php
@@ -29,7 +29,7 @@
     <title>Productos con Unidades <= <?= htmlspecialchars($tope) ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
     <script>
-        function modifyProduct() {
+        function show() {
             // Se obtiene el id de la fila donde está el botón presionado
             var rowId = event.target.parentNode.parentNode.id;
 
@@ -37,20 +37,50 @@
             var data = document.getElementById(rowId).querySelectorAll(".row-data");
 
             // Obtener los valores de cada columna relevante
-            var id = data[0].innerHTML;
-            var nombre = data[1].innerHTML;
-            var marca = data[2].innerHTML;
-            var modelo = data[3].innerHTML;
-            var precio = data[4].innerHTML;
-            var unidades = data[5].innerHTML;
-            var detalles = data[6].innerHTML;
+            var id = document.getElementById(rowId).querySelector("th").innerHTML;
+            var name = data[0].innerHTML;
+            var brand = data[1].innerHTML;
+            var model = data[2].innerHTML;
+            var price = data[3].innerHTML;
+            var units = data[4].innerHTML;
+            var details = data[5].innerHTML;
+            var image = document.getElementById(rowId).querySelector("img"); // Verificar si existe una imagen
+            var imageUrl = image ? image.src : "";
 
-            // Abrir el formulario de modificación con los datos del producto
-            var urlForm = "formulario_productos_v2.html";
-            window.open(urlForm + "?id=" + id + "&nombre=" + nombre + "&marca=" + marca +
-                        "&modelo=" + modelo + "&precio=" + precio + "&unidades=" + unidades +
-                        "&detalles=" + detalles, "_blank");
+             // Mostrar una alerta con el nombre del producto
+             alert("Nombre: " + name + " ID: " + id);
+
+            // Llamar a la función para enviar los datos al formulario
+            send2form(id, name, brand, model, price, units, details, imageUrl);
         }
+
+        function createHiddenInput(name, value) {
+            var input = document.createElement("input");
+            input.type = 'hidden';
+            input.name = name;
+            input.value = value;
+            return input;
+        }
+
+        function send2form(id, name, brand, model, price, units, details, imageUrl) {
+        var form = document.createElement("form");
+        form.method = 'POST';
+        form.action = "http://localhost/tecweb/practicas/p10/formulario_productos_v2.php";
+
+        form.appendChild(createHiddenInput('id', id));
+        form.appendChild(createHiddenInput('nombre', name));
+        form.appendChild(createHiddenInput('marca', brand));
+        form.appendChild(createHiddenInput('modelo', model));
+        form.appendChild(createHiddenInput('precio', price));
+        form.appendChild(createHiddenInput('unidades', units));
+        form.appendChild(createHiddenInput('detalles', details));
+        form.appendChild(createHiddenInput('imagen', imageUrl));
+
+        document.body.appendChild(form);
+        form.submit();
+}
+
+
     </script>
 </head>
 <body>
@@ -58,11 +88,11 @@
 
     <br />
 
-    <?php if (isset($productos) && count($productos) > 0) : ?>
+     <?php if (isset($productos) && count($productos) > 0) : ?>
         <table class="table table-striped">
             <thead class="thead-dark">
                 <tr>
-                    <th scope="col">ID</th>
+                    <th scope="col">#</th>
                     <th scope="col">Nombre</th>
                     <th scope="col">Marca</th>
                     <th scope="col">Modelo</th>
@@ -74,17 +104,17 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($productos as $producto) : ?>
-                    <tr id="producto-<?= htmlspecialchars($producto['id']) ?>">
-                        <td class="row-data"><?= htmlspecialchars($producto['id']) ?></td>
+                <?php foreach ($productos as $index => $producto) :?>
+                    <tr id="row-<?= $index ?>">
+                        <th scope="row"><?= htmlspecialchars($producto['id']) ?></th>
                         <td class="row-data"><?= htmlspecialchars($producto['nombre']) ?></td>
                         <td class="row-data"><?= htmlspecialchars($producto['marca']) ?></td>
                         <td class="row-data"><?= htmlspecialchars($producto['modelo']) ?></td>
                         <td class="row-data"><?= htmlspecialchars($producto['precio']) ?></td>
                         <td class="row-data"><?= htmlspecialchars($producto['unidades']) ?></td>
-                        <td class="row-data"><?= htmlspecialchars(utf8_encode($producto['detalles'])) ?></td>
+                        <td class="row-data"><?= utf8_encode($producto['detalles']) ?></td>
                         <td><img src="<?= htmlspecialchars($producto['imagen']) ?>" alt="Imagen de <?= htmlspecialchars($producto['nombre']) ?>" style="width:100px;" /></td>
-                        <td><input type="button" value="Modificar" onclick="modifyProduct()" /></td>
+                        <td><input type="button" value="Modificar" onclick="show()" /></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
